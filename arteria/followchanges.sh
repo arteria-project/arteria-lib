4a0d82dcdd5b31302d338f2ee5d1ba38dd747647
withrocks <steinar.sturlaugsson@medsci.uu.se>
2015-06-17 11:06:42 +0200

    Minor change to followchanges

--- arteria/followchanges.sh
+++ arteria/followchanges.sh
@@ -1,1 +1,1 @@
-when-changed arteria_services.py arteria_services_tests.py -c "clear && python arteria_services_tests.py"
+when-changed runfolder.py runfolder_tests.py -c "clear && python runfolder_tests.py"
