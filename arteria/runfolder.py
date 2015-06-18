bc4e386033fb69a25580622f55eaddbe9c9ec7a8
withrocks <steinar.sturlaugsson@medsci.uu.se>
2015-06-18 10:19:22 +0200

    Formatting only

--- arteria/runfolder.py
+++ arteria/runfolder.py
@@ -1,7 +1,7 @@
 import time
 import os.path
 import requests
-from arteria.configuration import ConfigurationService  
+from arteria.configuration import ConfigurationService
 from arteria.logging import Logger
 import jsonpickle
 import socket
@@ -24,17 +24,17 @@ class RunfolderInfo():
     def __init__(self, host, path, state):
         self.host = host
         self.path = path
-        self.state = state 
+        self.state = state
 
     def __str__(self):
         return "{0}: {1}@{2}".format(self.state, self.path, self.host)
 
 class RunfolderService():
-    """Watches a set of directories on the server and reacts when one of them 
+    """Watches a set of directories on the server and reacts when one of them
        has a runfolder that's ready for processing"""
 
-    def __init__(self, 
-                 configuration_svc=ConfigurationService(), 
+    def __init__(self,
+                 configuration_svc=ConfigurationService(),
                  logger=Logger()):
         self._configuration_svc = configuration_svc
         self._logger = logger
@@ -60,10 +60,10 @@ class RunfolderService():
             raise Exception("The path {0} is not being monitored".format(path))
 
         if not self._dir_exists(path):
-            raise Exception("Directory does not exist: '{0}'".format(path)) 
-        info = RunfolderInfo(self._host(), path, self.get_runfolder_state(path)) 
+            raise Exception("Directory does not exist: '{0}'".format(path))
+        info = RunfolderInfo(self._host(), path, self.get_runfolder_state(path))
         return info
-        
+
     def _get_runfolder_state_from_state_file(self, runfolder):
         state_file = os.path.join(runfolder, ".arteria/state")
         if self._file_exists(state_file):
@@ -77,12 +77,12 @@ class RunfolderService():
     def get_runfolder_state(self, runfolder):
         # If there exists a state file, it defines the state, otherwise
         # it's the existence of a marker from a sequencer
-        state = self._get_runfolder_state_from_state_file(runfolder)  
+        state = self._get_runfolder_state_from_state_file(runfolder)
         if state == RunfolderInfo.STATE_NONE:
             completed_marker = os.path.join(runfolder, "RTAComplete.txt")
             ready = self._file_exists(completed_marker)
             if ready:
-                state = RunfolderInfo.STATE_READY 
+                state = RunfolderInfo.STATE_READY
 
         return state
 
@@ -93,7 +93,7 @@ class RunfolderService():
             os.makedirs(arteria_dir)
         with open(state_file, 'w') as f:
             f.write(state)
-        
+
     def is_runfolder_ready(self, directory):
         state = self.get_runfolder_state(directory)
         self._logger.debug("Checking {0}. state={1}".format(directory, state))
