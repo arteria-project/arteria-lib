054f2383c025ba9ed9fd6c8fa56ad1fb6eb07ed5
withrocks <steinar.sturlaugsson@medsci.uu.se>
2015-07-31 17:18:47 +0200

    Reading API documentation from docstrings
    Automatically reading the documentation for the /api endpoint from docstrings

--- arteria/arteria/web.py
+++ arteria/arteria/web.py
@@ -1,56 +1,100 @@
 import tornado.web
 import jsonpickle
+import re
+import threading
+
+def undocumented(f):
+    """
+    Apply the undocumented decorator to handler methods if they should not turn up in the API help
+    """
+    f.undocumented = True
+    return f
 
 class TornadoAppFactory:
     """
     Helpers for a Tornado app
     """
     @staticmethod
-    def create_app(debug, routes_with_help):
+    def create_app(debug, routes):
         """Creates a web app for serving the REST endpoints, adding defaults for administration"""
-        route_provider = RouteProvider(routes_with_help)
-        app = tornado.web.Application(route_provider.get_endpoints(), debug=debug)
+        route_provider = RouteProvider(routes, debug)
+        app = tornado.web.Application(route_provider.get_routes(), debug=debug)
         return app
 
+class RouteInfo:
+    def __init__(self, route, method, description):
+        self.route = route
+        self.method = method
+        self.description = description
+
+    def __repr__(self):
+        return "[{0} method={1}: {2}]".format(self.route, self.method, self.description)
+
 class RouteProvider:
-    def __init__(self, routes_with_help):
+    def __init__(self, routes, debug):
         """
         Initialize with routes and related help
 
-        :param routes_with_help: A list of tuples with tornado routing definitions as first elements
-        and a help string as the second
+        :param routes: A list of tuples with tornado routing definitions
+        :param debug: True if debugging
         """
-        self._routes_with_help = routes_with_help
-        self._routes_with_help.extend(self._get_default_routes(self))
+        self._debug = debug
+        self._routes = routes
+        self._routes.extend(self._get_default_routes(self))
+        self._route_infos = sorted(self._get_route_infos(self._routes),
+                                   key=lambda entry: (entry.route, entry.method))
+        self._base_url_applied = False
+        # TODO: Review locking code
+        self._base_url_applied_lock = threading.Lock()
 
-    def get_endpoints(self):
-        endpoints = [entry[0] for entry in self._routes_with_help]
-        return endpoints
+    def _doc_string_from_class(self, cls, attr_name):
+        attr = getattr(cls, attr_name)
+        doc = attr.__doc__
+        is_undocumented = hasattr(attr, "undocumented")
+        if doc is not None and (not is_undocumented or self._debug):
+            doc = doc.strip()
+            doc = re.sub(r'\s+', ' ', doc)
+            if is_undocumented:
+                doc = "(UNDOCUMENTED): {0}".format(doc)
+            return doc
+
+    def _get_route_infos(self, tornado_routes):
+        for tornado_route in tornado_routes:
+            route = tornado_route[0]
+            cls = tornado_route[1]
+            for method_name in "get", "post", "put", "delete":
+                doc = self._doc_string_from_class(cls, method_name)
+                if doc is not None:
+                    yield RouteInfo(route, method_name, doc)
+
+    def get_routes(self):
+        return self._routes
 
     def _map_to_help_entry(self, raw_entry, base_url):
         route = raw_entry[2] if len(raw_entry) > 2 else raw_entry[0][0]
         route = "{0}{1}".format(base_url, route)
         return {"route": route, "description": raw_entry[1]}
 
+    def _apply_base_url(self, base_url):
+        if not self._base_url_applied:
+            with self._base_url_applied_lock:
+                if not self._base_url_applied:
+                    for route_info in self._route_infos:
+                        route_info.route = "{0}{1}".format(base_url, route_info.route)
+                    self._base_url_applied = True
+
     def get_help(self, base_url):
-        mapped = (self._map_to_help_entry(entry, base_url) for entry in self._routes_with_help)
-        filtered = (entry for entry in mapped if entry["description"] is not None)
-        help_doc = sorted(filtered, key=lambda entry: entry["route"])
-        return help_doc
+        # Apply the base_url the first time we get a request
+        self._apply_base_url(base_url)
+        return self._route_infos
 
     def _get_default_routes(self, route_provider):
         """
         Gets the default endpoints for a web service in the Arteria project
         """
         return [
-            (
-                (r"/api", ApiHelpHandler, dict(route_provider=route_provider)),
-                "Provides this help listing"
-            ),
-            (
-                (r"/api/1.0/admin/log_level", LogLevelHandler),
-                "Put {log_level: new_level} to update the log_level for the lifetime of the process"
-            )
+            (r"/api", ApiHelpHandler, dict(route_provider=route_provider)),
+            (r"/api/1.0/admin/log_level", LogLevelHandler)
         ]
 
 class BaseRestHandler(tornado.web.RequestHandler):
@@ -82,11 +126,20 @@ class BaseRestHandler(tornado.web.RequestHandler):
         return "%s://%s/api/%s" % (self.request.protocol, self.request.host, version)
 
 class LogLevelHandler(BaseRestHandler):
+    """
+    Handles getting/setting the log_level of the running application
+    """
     def get(self):
+        """
+        Get the current log_level of the running server
+        """
         log_level = self.admin_svc.get_log_level()
         self.write_object({"log_level": log_level})
 
     def put(self):
+        """
+        Set the current log_level of the running server. Call with e.g. {"log_level": "DEBUG"}
+        """
         json_body = self.body_as_object(["log_level"])
         log_level = json_body["log_level"]
         self.admin_svc.set_log_level(log_level)
@@ -100,6 +153,7 @@ class ApiHelpHandler(BaseRestHandler):
         self.route_provider = route_provider
 
     def get(self):
+        """Returns the help for the API"""
         base_url = "{0}://{1}".format(self.request.protocol, self.request.host)
         help_doc = self.route_provider.get_help(base_url)
         self.write_object(help_doc)
--- runfolder/runfolder/runfolder_ws.py
+++ runfolder/runfolder/runfolder_ws.py
@@ -13,8 +13,7 @@ class BaseRunfolderHandler(BaseRestHandler):
         runfolder_info.link = self.create_runfolder_link(runfolder_info.path)
 
     def create_runfolder_link(self, path):
-        return "%s/runfolders/path%s" % (
-            self.api_link(), path)
+        return "{0:s}/runfolders/path{1:s}".format(self.api_link(), path)
 
     def initialize(self, admin_svc, runfolder_svc, config_svc):
         self.admin_svc = admin_svc
@@ -22,8 +21,8 @@ class BaseRunfolderHandler(BaseRestHandler):
         self.config_svc = config_svc
 
 class ListAvailableRunfoldersHandler(BaseRunfolderHandler):
-    """List all available runfolders"""
     def get(self):
+        """List all available runfolders"""
         runfolder_infos = list(self.runfolder_svc.list_available_runfolders())
         for runfolder_info in runfolder_infos:
             self.append_runfolder_link(runfolder_info)
@@ -32,6 +31,7 @@ class ListAvailableRunfoldersHandler(BaseRunfolderHandler):
 
 class NextAvailableRunfolderHandler(BaseRunfolderHandler):
     def get(self):
+        """Returns the next runfolder to process"""
         runfolder_info = self.runfolder_svc.next_runfolder()
         self.append_runfolder_link(runfolder_info)
         self.write_object(runfolder_info)
@@ -40,6 +40,11 @@ class RunfolderHandler(BaseRunfolderHandler):
     """Handles a particular runfolder"""
 
     def get(self, path):
+        """
+        Returns information about the runfolder at the path.
+
+        The runfolder must a subdirectory of a monitored path.
+        """
         try:
             runfolder_info = self.runfolder_svc.get_runfolder_by_path(path)
             self.append_runfolder_link(runfolder_info)
@@ -52,46 +57,32 @@ class RunfolderHandler(BaseRunfolderHandler):
     def post(self, path):
         self.runfolder_svc.set_runfolder_state(path, "TODO")
 
+    @arteria.web.undocumented
     def put(self, path):
-        """NOTE: put is provided for test purposes only. TODO: Discuss if
-        it should be disabled in production"""
+        """
+        NOTE: put is provided for test purposes only.
+
+        TODO: Discuss if it should be disabled in production
+        """
         try:
             self.runfolder_svc.create_runfolder(path)
         except PathNotMonitored:
             raise tornado.web.HTTPError("400", "Path {0} is not monitored".format(path))
 
-
-
 class TestFakeSequencerReadyHandler(BaseRunfolderHandler):
+    @arteria.web.undocumented
     def put(self, path):
+        """
+        Marks the runfolder at the path as ready
+        """
         self.runfolder_svc.add_sequencing_finished_marker(path)
 
-def create_app(debug, args):
-    # Define the endpoints as tuples expected by Tornado, but adding the help information:
-    # If there is no help string (undocumented feature) it will not be shown in the help
-    # A third entry in the tuple overrides the route in the help listing if provided
-
-    endpoints = [
-        (
-            (r"/api/1.0/runfolders", ListAvailableRunfoldersHandler, args),
-            "List all runfolders"
-        ),
-        (
-            (r"/api/1.0/runfolders/next", NextAvailableRunfolderHandler, args),
-            "Return the next runfolder to process"),
-        (
-            (r"/api/1.0/runfolders/path(/.*)", RunfolderHandler, args),
-            "Returns information about the runfolder at the path. The path must be monitored",
-            "/api/1.0/runfolders/path/root/monitored/directory"
-        ),
-        (
-            (r"/api/1.0/runfolders/test/markasready/path(/.*)", TestFakeSequencerReadyHandler, args),
-            None
-        ),
-    ]
-
-    app = arteria.web.TornadoAppFactory.create_app(debug, endpoints)
-    return app
+def _create_config(path):
+    from configuration import ConfigurationFile
+    dirs = ['/data/testarteria1/mon1', '/data/testarteria1/mon2']
+    config_file = ConfigurationFile(dirs, 10800)
+    ConfigurationFile.write(path, config_file)
+    print "Created default config file at {0}".format(path)
 
 @click.command()
 @click.option('--config', default="./runfolder.config")
@@ -114,16 +105,15 @@ def start(config, loggerconfig, debug, create_config):
     logger.info("Starting the runfolder micro service on {0} (debug={1})"
                 .format(config_svc.port(), debug))
     args = dict(runfolder_svc=runfolder_svc, admin_svc=admin_svc, config_svc=config_svc)
-    app = create_app(debug, args)
+    # Setup the routing. Help will be automatically available at /api, and will be based on
+    # the doc strings of the get/post/put/delete methods
+    routes = [
+        (r"/api/1.0/runfolders", ListAvailableRunfoldersHandler, args),
+        (r"/api/1.0/runfolders/next", NextAvailableRunfolderHandler, args),
+        (r"/api/1.0/runfolders/path(/.*)", RunfolderHandler, args),
+        (r"/api/1.0/runfolders/test/markasready/path(/.*)", TestFakeSequencerReadyHandler, args)
+    ]
+    app = arteria.web.TornadoAppFactory.create_app(debug, routes)
     app.listen(config_svc.port())
     tornado.ioloop.IOLoop.current().start()
 
-def _create_config(path):
-    from configuration import ConfigurationFile
-    dirs = ['/data/testarteria1/mon1', '/data/testarteria1/mon2']
-    config_file = ConfigurationFile(dirs, 10800)
-    ConfigurationFile.write(path, config_file)
-    print "Created default config file at {0}".format(path)
-
-if __name__ == "__main__":
-    start()
